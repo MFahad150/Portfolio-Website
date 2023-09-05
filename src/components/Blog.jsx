28d020e9057fe83c40cc07ad22436e5d456e6fef
fahad <mfahad1667@gmail.com>
2023-09-05 11:23:11 +0500

    update blog and projects components

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -13,22 +13,22 @@ const Blog = () => {
       <SectionTitle title="Blogs" titleNo="03" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         <BlogCard
-          title="Brief History of DevOps"
-          des="DevOps has grown since its initial conception in 2007-2008. DevOps is a shared philosophy in which development and operations teams work together."
-          listItem={["DevOps", "Timeline", "Patrick Debois"]}
-          link="https://medium.com/@mfahad1667/brief-history-of-devops-c295d54d099d"
+          title="SSH Connection Between Two Conatiners"
+          des="This project involves setting up a Secure Shell connection between two Ubuntu containers in Docker."
+          listItem={["Docker", "SSH", "Security"]}
+          link="https://medium.com/@mfahad1667/ssh-connection-between-two-docker-container-7c9dced1aa43"
+        />
+        <BlogCard
+          title="Object-Oriented Programming in TypeScript"
+          des="Object-Oriented Programming is a pragramming paradigm or style of programming that is centered on objects rather than functions."
+          listItem={["OOP", "TypeScript", "Concepts"]}
+          link="https://medium.com/@mfahad1667/navigating-the-world-of-object-oriented-programming-in-typescript-b7b2d18fc96a"
         />
         <BlogCard
           title="Infrastructure as Code (IaC)"
           des="Infrastructure as Code is the practice of describing infrastructure in executable textual format. "
           listItem={["Server", "YAML", "JSON"]}
           link="https://www.linkedin.com/posts/muhammad-fahad-b9a985220_management-kubernetes-software-activity-7059489352900317184-fouv?utm_source=share&utm_medium=member_desktop"
-        />
-        <BlogCard
-          title="Social Coding"
-          des="Social coding empowers programmers to work together efficiently, learn from one another, and create high-quality software."
-          listItem={["Pair Programming", "Open Source"]}
-          link="https://medium.com/@mfahad1667/social-coding-6eaa4e7dd2ed"
         />
         {showMore && (
           <>
@@ -38,10 +38,34 @@ const Blog = () => {
               transition={{ delay: 0.1 }}
             >
               <BlogCard
+                title="Brief History of DevOps"
+                des="DevOps has grown since its initial conception in 2007-2008. DevOps is a shared philosophy in which development and operations teams work together."
+                listItem={["DevOps", "Timeline", "Patrick Debois"]}
+                link="https://medium.com/@mfahad1667/brief-history-of-devops-c295d54d099d"
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
+              <BlogCard
                 title="Test-Driven Development (TDD)"
                 des="Test-Driven Development (TDD) is a methodology in software development that focuses on creating unit test cases before developing the actual code. "
                 listItem={["Clean Code", "Test Cases"]}
                 link="https://www.linkedin.com/posts/muhammad-fahad-b9a985220_devops-testdrivendevelopment-quality-activity-7062776737159426049-j32H?utm_source=share&utm_medium=member_desktop"
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ delay: 0.3 }}
+            >
+              <BlogCard
+                title="Social Coding"
+                des="Social coding empowers programmers to work together efficiently, learn from one another, and create high-quality software."
+                listItem={["Pair Programming", "Open Source"]}
+                link="https://medium.com/@mfahad1667/social-coding-6eaa4e7dd2ed"
               />
             </motion.div>
           </>
